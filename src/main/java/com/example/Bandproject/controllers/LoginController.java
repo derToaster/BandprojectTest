ef64aee2870d550b77c942b94724afea0e8ff9e5
derToaster <ashkanmoradi1412@yahoo.de>
2020-09-15 18:21:25 +0200

    working version login simple

---
+++ src/main/java/com/example/Bandproject/controllers/LoginController.java
@@ -0,0 +1,28 @@
+//package com.example.Bandproject.controllers;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.servlet.ModelAndView;
+//
+//
+//@Controller
+//@RequestMapping("/logger")
+//public class LoginController {
+//
+//
+//    @PostMapping
+//    public ModelAndView loggedinPage () {
+//        ModelAndView modelAndView = new ModelAndView("Userpage");
+//        return modelAndView;
+//    }
+//    @GetMapping
+//    public ModelAndView loginPage () {
+//        ModelAndView modelAndView = new ModelAndView("login");
+//        return modelAndView;
+//    }
+//
+//
+//}
--- src/main/java/com/example/Bandproject/services/Security.java
+++ src/main/java/com/example/Bandproject/services/Security.java
@@ -2,6 +2,7 @@ package com.example.Bandproject.services;
 
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,19 +10,21 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
+@Configuration
 @EnableWebSecurity
 public class Security extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests().antMatchers("/registration/all", "/Success", "/registration").permitAll()
+                .authorizeRequests().antMatchers("/registration").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .formLogin().loginPage("/login").permitAll()
-                .and()
-                .httpBasic();
+                .formLogin()
+                    .loginPage("/login").permitAll()
+                    .and()
+                .logout()
+                    .permitAll();
 
     }
         @Bean
